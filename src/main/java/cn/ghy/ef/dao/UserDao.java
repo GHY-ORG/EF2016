4cf1fdd3d077baded56ac2289f193b8949a80923
SandBoat <695514503@qq.com>
2016-08-11 21:18:03 +0800

    为 UserDao 添加简单的增删改

--- src/main/java/cn/ghy/ef/dao/UserDao.java
+++ src/main/java/cn/ghy/ef/dao/UserDao.java
@@ -18,7 +18,7 @@ public class UserDao {
 	// Mapper
 	private static userMapper mapper = sqlSession.getMapper(userMapper.class);
 
-	// select * from ef_user where status = 0 or 1
+	// select * from user where status = 0 or 1
 	public static List<user> getAll() throws SQLException {
 		try {
 			userExample example = new userExample();
@@ -28,6 +28,89 @@ public class UserDao {
 			example.or().andStatusIn(values);
 
 			return mapper.selectByExample(example);
+
+		} finally {
+			sqlSession.close();
+		}
+	}
+
+	// select * from user where status = ?
+	public static List<user> getAllByStatus(int status) throws SQLException {
+		try {
+			userExample example = new userExample();
+			example.or().andStatusEqualTo(status);
+
+			return mapper.selectByExample(example);
+
+		} finally {
+			sqlSession.close();
+		}
+	}
+
+	// select * from user where id = ? and status = ?
+	public static List<user> getOneById(int id, int status) throws SQLException {
+		try {
+			userExample example = new userExample();
+			example.or().andStatusEqualTo(id);
+
+			return mapper.selectByExample(example);
+
+		} finally {
+			sqlSession.close();
+		}
+	}
+
+	// if(nickname == "")select * from user where username = ?
+	// if(username == "")select * from user where nickname = ?
+	// select * from user where username = ? and nickname = ?
+	public static List<user> getUserList(String username, String nickname) throws SQLException {
+		try {
+			userExample example = new userExample();
+
+			if (username != "" && nickname != "") {
+				example.or().andUsernameEqualTo(username).andNicknameEqualTo(nickname);
+			} else if (username != "") {
+				example.or().andNicknameEqualTo(username);
+			} else {
+				example.or().andNicknameEqualTo(nickname);
+			}
+
+			return mapper.selectByExample(example);
+		} finally {
+			sqlSession.close();
+		}
+	}
+
+	// insert into
+	// user(nickname,username,password,sex,phone,email,stunumber,registertime,status)
+	// values (?,?,?,?,?,?,?,?,?)
+	public static boolean add(user record) throws SQLException {
+
+		try {
+
+			if (mapper.insert(record) > 0) {
+				return true;
+			} else {
+				return false;
+			}
+
+		} finally {
+			sqlSession.close();
+		}
+	}
+
+	// update user
+	// set nickname = ?,username = ?,password = ?,sex = ?,phone = ?,email =
+	// ?,stunumber = ?,status = ?
+	// where userid = ?
+	public static boolean updateById(user record) throws SQLException {
+		try {
+
+			if (mapper.updateByPrimaryKeySelective(record) > 0) {
+				return true;
+			} else {
+				return false;
+			}
 
 		} finally {
 			sqlSession.close();
